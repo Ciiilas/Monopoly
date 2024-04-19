0695c410c897e5c166b28bad0d38ab152451c0d0
Cilas Handloser <cilashandloser89@gmail.com>
2024-04-19 11:30:53 +0200

    TUI erweitert/verbessert

--- src/main/scala/de/htwg/se/monopoly/TUI.scala
+++ src/main/scala/de/htwg/se/monopoly/TUI.scala
@@ -2,6 +2,11 @@ package de.htwg.se.monopoly
 
 case class TUI()
 
+def playingfield(): Unit = {
+  barTop()
+  cells()
+  barBottom()
+}
 def playingfield(board_size: Int,card_size_x: Int, card_size_y: Int): Unit = {
   barTop(board_size, card_size_x)
   cells(board_size,card_size_x, card_size_y)
@@ -14,9 +19,18 @@ def barTop(board_size: Int, card_size_x: Int): Unit = {
   println("┌" + ("─" * card_size_x + "┬") * (board_size - 1) + "─" * card_size_x + "┐")
 }
 
+def cells(): Unit = {
+  for (a <- 1 to 9)
+    println("│" + (" " * 10 + "│") * (36 - 1) + " " * 10 + "│")
+}
+
 def cells(board_size: Int, card_size_x: Int, card_size_y: Int): Unit = {
   for (a <- 1 to card_size_y)
     println("│" + (" " * card_size_x + "│") * (board_size - 1) + " " * card_size_x + "│")
+}
+
+def barBottom(): Unit = {
+  println("└" + ("─" * 10 + "┴") * (36 - 1) + "─" * 10 + "┘")
 }
 
 def barBottom(board_size: Int, card_size_x: Int): Unit = {
