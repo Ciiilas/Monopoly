068439e20aa4640af238cc743fce95402d520cd6
Cilas Handloser <cilashandloser89@gmail.com>
2024-04-19 08:54:34 +0200

    main in test zum testen verschoben

--- src/test/scala/de/htwg/se/monopoly/MySuite.scala
+++ src/test/scala/de/htwg/se/monopoly/MySuite.scala
@@ -1,9 +1,9 @@
-// For more information on writing tests, see
-// https://scalameta.org/munit/docs/getting-started.html
-class MySuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
-    assertEquals(obtained, expected)
-  }
-}
+@main def monopoly(): Unit =
+  val board_size = 5
+  val card_size_x = 7
+  val card_size_y = 8
+
+  println("┌" + ("─"* card_size_x + "┬")*(board_size-1) + "─"* card_size_x + "┐")
+  for(a <- 1 to card_size_y)
+    println("│" + (" "* card_size_x + "│" )*(board_size-1) + " "*card_size_x + "│")
+  println("└" + ("─"* card_size_x + "┴")*(board_size-1) + "─"* card_size_x + "┘")
