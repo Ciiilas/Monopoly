f0ebfd56e15b14a95c0687410650e2267a2bfffe
lememin <164020341+lememin@users.noreply.github.com>
2024-04-19 12:44:28 +0200

    modified data

--- src/main/scala/de/htwg/se/monopoly/data.scala
+++
@@ -1,6 +0,0 @@
-package de.htwg.se.monopoly
-
-case class data(player1_position: Int, player2_position: Int, player3_position: Int, player4_position: Int,
-                player1_coins: Int, player2_coins: Int, player3_coins: Int, player4_coins: Int)
-
-
