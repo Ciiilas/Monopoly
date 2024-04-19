84776a76924bcc97d623160c757dfa6ee77de182
lememin <164020341+lememin@users.noreply.github.com>
2024-04-19 09:34:00 +0200

    added case class TUI and copy code from main to it

--- src/main/scala/de/htwg/se/monopoly/Main.scala
+++ src/main/scala/de/htwg/se/monopoly/Main.scala
@@ -1,11 +1,2 @@
 package de.htwg.se.monopoly
 @main def monopoly(): Unit =
-  val board_size = 5
-  val card_size_x = 7
-  val card_size_y = 8
-
-  println("┌" + ("─"* card_size_x + "┬")*(board_size-1) + "─"* card_size_x + "┐")
-
-  for(a <- 1 to card_size_y)
-    println("│" + (" "* card_size_x + "│" )*(board_size-1) + " "*card_size_x + "│")
-  println("└" + ("─"* card_size_x + "┴")*(board_size-1) + "─"* card_size_x + "┘")
---
+++ src/main/scala/de/htwg/se/monopoly/TUI.scala
@@ -0,0 +1,13 @@
+package de.htwg.se.monopoly
+
+case class TUI()
+
+val board_size = 5
+val card_size_x = 7
+val card_size_y = 8
+
+println("┌" + ("─" * card_size_x + "┬") * (board_size - 1) + "─" * card_size_x + "┐")
+
+for (a <- 1 to card_size_y)
+  println("│" + (" " * card_size_x + "│") * (board_size - 1) + " " * card_size_x + "│")
+println("└" + ("─" * card_size_x + "┴") * (board_size - 1) + "─" * card_size_x + "┘")
