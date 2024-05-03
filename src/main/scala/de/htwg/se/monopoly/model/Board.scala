9be8d54cdae9ab93d36d36976081929347922fb5
lememin <164020341+lememin@users.noreply.github.com>
2024-05-03 12:55:28 +0200

    not sure what i did since last commit

--- src/main/scala/de/htwg/se/monopoly/model/Board.scala
+++ src/main/scala/de/htwg/se/monopoly/model/Board.scala
@@ -1,9 +1,10 @@
 package de.htwg.se.monopoly.model
 import de.htwg.se.monopoly.model.Player
+import scala.collection.immutable.Queue
 
-case class Board(players: Map[Figure, Player], cards: Map[Int, Card], dice: Dice ) {
+case class Board(players: Queue[Player], cards: Map[Int, Card], turn: Int) {
 
-  def walk(player: Figure, number_of_fields: Int): Board = {
+  def walk(): Board = {
     this.copy(players.get(player).get.set_position(number_of_fields))
 
   }
@@ -11,3 +12,7 @@ case class Board(players: Map[Figure, Player], cards: Map[Int, Card], dice: Dice
  
 
 }
+
+
+
+
