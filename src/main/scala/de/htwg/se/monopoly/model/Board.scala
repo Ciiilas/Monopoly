f373005173a44e9fc2c05fa7c8229d3f863c2ef9
Cilas Handloser <cilashandloser89@gmail.com>
2024-05-03 20:38:32 +0200

    board changes

--- src/main/scala/de/htwg/se/monopoly/model/Board.scala
+++ src/main/scala/de/htwg/se/monopoly/model/Board.scala
@@ -5,23 +5,23 @@ import scala.collection.immutable.Queue
 
 case class Board(players: Queue[Player], cards: Map[Int, Card], dice: Int) {
 
+  val size = cards.size
   val eol = sys.props("line.separator")
 
   def barTop(board_size: Int = 10, card_size_x: Int = 15): String = {
-    "┌" + ("─" * card_size_x + "┬") * (board_size - 1) + "─" * card_size_x + "┐"
+    "┌" + ("─" * card_size_x + "┬") * (board_size - 1) + "─" * card_size_x + "┐" + eol
   }
 
-  def cell(board_size: Int = 10, card_size_x: Int = 15): String = {
-    "│" + (" " * card_size_x + "│") * (board_size - 1) + " " * card_size_x + "│"
+  def cell(board_size: Int = 10, card_size_x: Int = 15) = "│" + (" " * card_size_x + "│") * (board_size - 1) + " " * card_size_x + "│" + eol
   }
 
   def barBottom(board_size: Int, card_size_x: Int): String = {
     "└" + ("─" * card_size_x + "┴") * (board_size - 1) + "─" * card_size_x + "┘"
   }
-  
-  
+
+
   def walk(): Board = {
-    
+
     //this.copy(players.head.set_position(dice))
   }
 
