47a340817b34122640920751814b54e6ddb4e794
lememin <164020341+lememin@users.noreply.github.com>
2024-05-03 11:40:42 +0200

    started added walk funktion

--- src/main/scala/de/htwg/se/monopoly/controller/Controller.scala
+++ src/main/scala/de/htwg/se/monopoly/controller/Controller.scala
@@ -4,9 +4,20 @@ import de.htwg.se.monopoly.model
 import de.htwg.se.monopoly.model.{Board, Board_output_on_console, Dice}
 import de.htwg.se.monopoly.util.Observable
 
-class Controller extends Observable{
+class Controller(var board: Board) extends Observable{
+
+  def createBoard(size: Int): Unit = {
+    board = new Board(size);
+
+  }
   def getDice():Unit = {
     val dice = new Dice
     notifyObservers
   }
+
+  def walk(): Unit = {
+
+  }
+
+
 }
