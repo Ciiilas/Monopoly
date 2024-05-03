0814e11b4fb7b59c22779e775a5db95e0e5db1f9
Cilas Handloser <cilashandloser89@gmail.com>
2024-05-03 11:41:53 +0200

    fehler

--- src/main/scala/de/htwg/se/monopoly/model/Board.scala
+++ src/main/scala/de/htwg/se/monopoly/model/Board.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.monopoly.model
 
-case class Board(players: Map[String, Player], cards: Map[Int, Card] ) {
+case class Board(players: Map[String, Player], cards: Map[Int, Card], dice: Dice ) {
 
   def walk(Boardold: Board, player: String, number_of_fields: Int): Board = {
     val Board = Board();
@@ -8,6 +8,6 @@ case class Board(players: Map[String, Player], cards: Map[Int, Card] ) {
 
   }
 
-
+ 
 
 }
