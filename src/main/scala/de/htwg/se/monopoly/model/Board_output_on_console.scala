e98a38e62ac3147cc2dcaa0dab7c4e9c9bb830f0
lememin <164020341+lememin@users.noreply.github.com>
2024-05-03 09:00:19 +0200

    renamed Board to Board_output_on_console

---
+++ src/main/scala/de/htwg/se/monopoly/model/Board_output_on_console.scala
@@ -0,0 +1,25 @@
+package de.htwg.se.monopoly.model
+
+import scala.math.Equiv
+
+class Board_output_on_console {
+
+def playingfield(board_size: Int,card_size_x: Int, card_size_y: Int): Unit = {
+  println(barTop(board_size, card_size_x))
+  for (a <- 1 to card_size_y)
+    println(cell(board_size, card_size_x))
+  println(barBottom(board_size,card_size_x))
+}
+
+def barTop(board_size: Int, card_size_x: Int): String = {
+  "┌" + ("─" * card_size_x + "┬") * (board_size - 1) + "─" * card_size_x + "┐"
+}
+
+def cell(board_size: Int, card_size_x: Int): String = {
+  "│" + (" " * card_size_x + "│") * (board_size - 1) + " " * card_size_x + "│"
+}
+
+def barBottom(board_size: Int, card_size_x: Int): String = {
+  "└" + ("─" * card_size_x + "┴") * (board_size - 1) + "─" * card_size_x + "┘"
+}
+}
