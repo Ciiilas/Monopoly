6e5c44fdcda273762845b30ab0ceb55956bb1eb5
Cilas Handloser <cilashandloser89@gmail.com>
2024-05-17 09:56:06 +0200

    Constructor changes

--- src/main/scala/de/htwg/se/monopoly/model/Card.scala
+++ src/main/scala/de/htwg/se/monopoly/model/Card.scala
@@ -1,5 +1,8 @@
 package de.htwg.se.monopoly.model
 
-case class Card(name: Street_Names, houses: Int, owner: Figure) {
+import de.htwg.se.monopoly.model.Street_Names.Empty
 
+case class Card(name: Street_Names, houses: Int, owner: Figure) {
+ def this() = this(Empty, 0, Figure.Empty)
+ 
 }
